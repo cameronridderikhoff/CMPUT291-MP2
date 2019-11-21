4a0e79d6f6c05de17427284dc96791df0f7da6ac
cameronridderikhoff <cameronridderikhoff@gmail.com>
2019-11-21 16:09:55 -0700

    Initial commit, setting up repo

---
+++ menu.py
@@ -0,0 +1,121 @@
+class menu:
+    def __init__(self):
+        self.star = "*************************"
+        super().__init__()
+    
+    # This method controls the users queries to the database index files.
+    # It interacts with the user to get their queries, and converts the plain text input into a 
+    # format useable by the methods we are producing.
+    # Possible query entries: date, subject, body, from, to, cc and bcc
+    def main_menu(self):
+        print(self.star)
+        print("Welcome to the Mini Project 2")
+        print("Press e at any time to exit.")
+        print(self.star)
+        i = input("Please enter your query: ")
+        while i.lower() != "e":
+            q = i.lower().split() # splits by whitespace, and there must be a space between each item of the query
+            query = []
+            for j in range(len(q)):
+                # combine the query commands with their respective symbols
+                # Eg. "date     :     2019/10/10 becomes ["date:", "2019/10/10"]
+                if q[j] == ":" or q[j] == ">" or q[j] == "<" or q[j] == ">=" or q[j] == "<=":
+                    query.append(q[j-1] + q[j])
+                else:
+                    #check to see if the first character is a symbol, if it is, we need to append the symbol to the previous entry
+                    #and remove it from the current entry
+                    if q[j][0] == ":":
+                        query.pop()
+                        query.append(q[j-1] + ":")
+                        q[j] = q[j][1:]
+                    elif q[j][0] == "<":
+                        if  q[j][1] == "=":
+                            q[j] = q[j][2:]
+                        else:
+                            q[j] = q[j][1:]
+                        query.pop()
+                        query.append(q[j-1] + ":")
+                        
+                    elif q[j][0] == ">":
+                        if q[j][1] == "=":
+                            q[j] = q[j][2:]
+                        else:
+                            q[j] = q[j][1:]
+                    #split the : using commands by their colon, and the <, >, >=, <= commands (date) by that character
+                    if not (q[j] == "date" or q[j] == "subj" or q[j] == "body" or q[j] == "from" or q[j] == "to" or q[j] == "cc" or q[j] == "bcc"):
+                        if len(q[j].split(":")) > 1:
+                            query.append(q[j].split(":")[0] + ":")
+                            query.append(q[j].split(":")[1])
+                        #must check ">=" and "<=" first, since they will also have a result of len() > 1 if we split them on ">" or "<"
+                        elif len(q[j].split(">=")) > 1:
+                            query.append(q[j].split(">=")[0] + ">=")
+                            query.append(q[j].split(">=")[1])
+                        elif len(q[j].split("<=")) > 1:
+                            query.append(q[j].split("<=")[0] + "<=")
+                            query.append(q[j].split("<=")[1])
+                        elif len(q[j].split(">")) > 1:
+                            query.append(q[j].split(">")[0] + ">")
+                            query.append(q[j].split(">")[1])
+                        elif len(q[j].split("<")) > 1:
+                            query.append(q[j].split("<")[0] + "<")
+                            query.append(q[j].split("<")[1])
+                        else:
+                            query.append(q[j])
+            #TODO remove
+            print(query)
+
+            #used to let the system know what the next item will be
+            next_item = ""
+
+            date = ""
+            date_operator = ""
+            subject = ""
+            body = ""
+            from_who = ""
+            to_who = ""
+            cc = ""
+            bcc = ""
+            for item in query:
+                if next_item == "d":
+                    date = item
+                elif next_item =="s":
+                    subject = item
+                elif next_item == "b":
+                    body = item
+                elif next_item == "f":
+                    from_who = item
+                elif next_item == "t":
+                    to_who = item
+                elif next_item == "c":
+                    cc = item
+                elif next_item == "bc":
+                    bcc = item
+                next_item = "" #reset next_item
+
+                if "date:" in item or "date>" in item or "date<" in item:
+                    next_item = "d"
+                    date_operator = item.split("date")
+                elif "subj:" in item or "subject:" in item:
+                    next_item = "s"
+                elif "body:" in item:
+                    next_item = "b"
+                elif "from:" in item:
+                    next_item = "f"
+                elif "to:" in item:
+                    next_item = "t"
+                elif "cc:" in item:
+                    next_item = "c"
+                elif "bcc:" in item:
+                    next_item = "bc"
+                else:
+                    if next_item == "":
+                        print("You have mistyped your query, please try again.")
+                        break
+            print(date, subject, body, from_who, to_who, cc, bcc)
+            i = input("Please enter your query, or press 'e' to exit: ")
+
+
+
+if __name__ == "__main__":
+    m = menu()
+    m.main_menu()
