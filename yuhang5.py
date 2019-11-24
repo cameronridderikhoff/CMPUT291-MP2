ce297cdfc8f5f7ae64a906f65c41ebaca7402663
yuhang5 <yuhang5@ualberta.ca>
2019-11-24 14:28:43 -0700

    yuhang5.py fix code and append row_id to list

--- yuhang5.py
+++ yuhang5.py
@@ -9,25 +9,19 @@ def get_emails_from(email):
     database.open("em.idx") 
     cur = database.cursor()
     result = cur.first()
-    #em=result[0].decode("utf-8")
-    #print(em)
     email_id = []
     while result:
         em=result[0].decode("utf-8")
         ids=result[1].decode("utf-8")
-        #print(em)
-        #print(ids)
+
         if ("from-" in em):
-            #print(em[5:])
             if (em[5:] == email):
-                #print(em)
                 e_id=ids
-                #print(e_id)
                 email_id.append(e_id)
                 print(em[5:],"|",e_id)
+                email_id.append(e_id)
         result = cur.next()
     
-        #print(em)
     cur.close()
     database.close() 
     
@@ -48,23 +42,28 @@ def get_emails_with_date(date, date_operator):
         if (':' in date_operator):
             if (time == date):
                 d_id = ids
+                date_id.append(d_id)
                 print(time,"|",d_id)
         elif ('<=' in date_operator):
             if  (time <= date):
                 d_id = ids 
+                date_id.append(d_id)
                 print(time,"|",d_id)
         elif ('<' in date_operator):
             if  (time < date):
-                d_id = ids       
+                d_id = ids
+                date_id.append(d_id)
                 print(time,"|",d_id)
-        elif ('>' in date_operator):
-            if  (time > date):
-                d_id = ids  
-                print(time,"|",d_id)
-        
         elif ('>=' in date_operator):
             if  (time >= date):
                 d_id = ids 
+                date_id.append(d_id)
+                print(time,"|",d_id)
+        
+        elif ('>' in date_operator):
+            if  (time > date):
+                d_id = ids 
+                date_id.append(d_id)
                 print(time,"|",d_id)
         result = cur.next()
     cur.close()
@@ -77,7 +76,7 @@ def get_emial_cc(email):
     result = cur.first()
     #em=result[0].decode("utf-8")
     #print(em)
-    email_id = []
+    cc_id = []
     while result:
         em=result[0].decode("utf-8")
         ids=result[1].decode("utf-8")
@@ -88,7 +87,7 @@ def get_emial_cc(email):
                 #print(em)
                 e_id=ids
                 #print(e_id)
-                email_id.append(e_id)
+                cc_id.append(e_id)
                 print(em[4:],"|",e_id)
         result = cur.next()
     
@@ -101,12 +100,14 @@ def get_email_with_body(body):
     database.open("te.idx")
     cur = database.cursor()
     result = cur.first() 
+    cody_id = []
     while result:
         bod=result[0].decode("utf-8")
         ids=result[1].decode("utf-8")
         if ( "b-" in bod):
             if (bod[2:] == body ):
                 b_id=ids
+                cody_id.append(b_id)
                 print(bod[2:],"|",b_id)
         result = cur.next()  
     cur.close()
