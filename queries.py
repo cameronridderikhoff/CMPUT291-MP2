5aa66a980b14e60b5d782c37b0e1337b3890bb00
cameronridderikhoff <cameronridderikhoff@gmail.com>
2019-11-24 23:43:53 -0700

    Updated queries.py
    Similarly to phase1.py, when doing merging, the get_emails_with_email function got overwritten with an old version.

--- queries.py
+++ queries.py
@@ -26,25 +26,22 @@ def get_emails_with_terms(terms, field):
 # field is the option for "from-", "cc-", "bcc-", "to-"
 # Using range search to get check does the input email which is plus to field and encode in our database.
 # If it in, get the row_id and append it to email_id list
-def get_emails_with_email(email, field):
-    database = db.DB()
-    database.open("em.idx") 
-    cur = database.cursor()
-    result = cur.first()
-    email_id = []
+def get_emails_with_email(email,field):
+       database = db.DB()
+       database.set_flags(db.DB_DUP)
+       database.open("em.idx",None, db.DB_BTREE, db.DB_CREATE) 
+       cur = database.cursor()
+       email_id = []    
+       result = cur.set_range((field + "-" + email).encode())
+       while (result !=None):
+              row = result[1].decode()
+              result = cur.next_dup()
+              email_id.append(row)                     
 
-    while result:
-        em=result[0].decode()
-        ids=result[1].decode()
-        if (field + "-" in em):
-            if (em[len(field)+1:] == email):
-                e_id = ids
-                email_id.append(e_id)
-        result = cur.next()
-    
-    cur.close()
-    database.close()
-    return email_id
+       
+       cur.close()
+       database.close()              
+       return email_id
 
 def show_rec(row, size):
     database = db.DB()
